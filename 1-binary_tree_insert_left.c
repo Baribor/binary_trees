51bb3b106618ff3df3d59ad151b848fd9799e5b7
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-01 14:57:26 +0100

    added both files to insert node on left and right of binary tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,9 +5,9 @@
  * @parent: is a pointer to the node to insert the left-child in
  * @value: is the valude to store in the new node
  *
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
  * Description: if parent has a left-child, the new node must take its place, and...
  * 		..the old left-child must be set as the left-child of the new node.
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
