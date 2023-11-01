de79da2a99994bf03dab0aacc0c7d1371a86d9a1
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-01 17:09:40 +0100

    modified the file function to insert a node as the left-child of a parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: is a pointer to the node to insert the left-child in
- * @value: is the valude to store in the new node
+ * @value: is the value to store in the new node
  *
  * Return: a pointer to the created node, or NULL on failure or if parent is
  * NULL
@@ -31,4 +31,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new_node;
 
 	return (new_node);
+
 }
