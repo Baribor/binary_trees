a2630a6607422a17ceecaca7784dfa261f5b7f2d
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-01 14:51:12 +0100

    added both files to insert node on left and right of binary tree

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: is a pointer to the node to insert the left-child in
+ * @value: is the valude to store in the new node
+ *
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Description: if parent has a left-child, the new node must take its place, and...
+ * 		..the old left-child must be set as the left-child of the new node.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return (NULL); /* if parent is NULL */
+	
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	
+	if (!new_node)
+		return (NULL); /* memory allocation failed */
+
+	/* if the parent already has a left child, the new node takes it place */
+	if (parent->left)
+	{
+		parent->left->parent = new_node;
+		new_node->left = parent->left;
+	}
+	
+	/* set new node as the left-child of the parent */
+	parent->left = new_node;
+
+	return (new_node);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: is a pointer to node to insert the right-child in
+ * @value: is the value to store in the new node
+ *
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * @Description: if parent already has a right-child, the new node must take..
+ * 		...its place,and the old right-child must be set as the...
+ * 		...right-child of the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return (NULL); /* if parent is NULL */
+
+	/* creating a new node */
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL); /* memory allocation failed */
+
+	/* if parent already has a right-child, the new node take its place */
+	if (parent->right)
+	{
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+	}
+
+	/* set the new node as the right-child of the parent */
+	parent->right = new_node;
+
+	return (new_node);
+}
