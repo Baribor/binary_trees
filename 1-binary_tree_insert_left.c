7f89ec2b7adbf4f1c9105d36cd648b3f97c22961
Baribor Saturday <saturdaybaribor@gmail.com>
2023-11-01 20:00:13 +0100

    Fixed betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL); /* if parent is NULL */
-	
+
 	/* creating a new node */
 	new_node = binary_tree_node(parent, value);
 	if (!new_node)
